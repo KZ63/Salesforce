4a973296fd5eb06a2c826c3df52edb2a45932a31
kayazutoma <kayazutoma@gmail.com>
2021-09-09 22:58:59 +0900

    add rowaction

--- force-app/main/default/lwc/opportunityTable/opportunityTable.js
+++ force-app/main/default/lwc/opportunityTable/opportunityTable.js
@@ -1,13 +1,20 @@
 import { LightningElement, wire, api } from 'lwc';
 import getOppTable from '@salesforce/apex/OpportunityTableController.getOppTable';
+import { NavigationMixin } from 'lightning/navigation';
+
+const actions = [
+  {label: 'Details', name: 'details'},
+  {label: 'Delete', name: 'delete'},
+];
 
 const columns = [
     { label: '商談名', fieldName: 'Name'},
     { label: '金額', fieldName: 'Amount', type: 'currency'},
-    { label: 'フェーズ', fieldName: 'StageName', type: 'text', editable: true}
+    { label: 'フェーズ', fieldName: 'StageName', type: 'text', editable: true},
+    { type: 'action', typeAttributes: {rowActions: actions, menuAlignment: 'right'}},
 ];
 
-export default class OpportunityTable extends LightningElement {
+export default class OpportunityTable extends NavigationMixin(LightningElement) {
 
   columns = columns;
   records;
@@ -17,6 +24,27 @@ export default class OpportunityTable extends LightningElement {
   wiredOpps({error, data}) {
     if(data) {
       this.records = data;
+    }
+  }
+
+  handleRowAction(event) {
+    console.log('■' + event.detail.action.name);
+    let actionName = event.detail.action.name;
+    let row = event.detail.row;
+    switch(actionName) {
+      case 'details':
+        // navigate to record detail page
+        this[NavigationMixin.Navigate]({
+          type: 'standard__recordPage',
+          attributes: {
+            recordId: row.Id,
+            actionName: 'view'
+          }
+        });
+        break;
+      case 'delete':
+        alert('delete but not really');
+        break;
     }
   }
 }
